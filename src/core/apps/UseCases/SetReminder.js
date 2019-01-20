aae01c04ea1f358c7c635b8b922b4b5c1a868061
LynxEyes <ivo.jesus@gmail.com>
2019-01-20 21:24:40 +0000

    The whole Reminders thing needs some re-thinking..

--- src/core/apps/UseCases/SetReminder.js
+++ src/core/apps/UseCases/SetReminder.js
@@ -12,6 +12,13 @@ export default (
     reminderId = setTimeout(sendNotification, remindIn)
   })
 
+  // TODO: This does not work properly. On node, the reminderId won't be a number
+  //       like it would on a browser, but a rather complex object. This object
+  //       cannot be serialized to a redis store and be brought back... a better
+  //       thing needs to be done...
+  //       Possibly just keeping remindersRepo as a in-memory thing with the ability
+  //       to store something that would be used to reinstate the timeout in case
+  //       the app crashes or gets restarted
   await remindersRepo.add(ctx.app, reminderId)
 
   return { ...ctx, reminderId, remindPromise }
