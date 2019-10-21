038ac022e3c429e64aab130842fd64ba2bdfbcc7
My Name Here <ivo.jesus@gmail.com>
2019-10-21 19:44:05 +0100

    Add client build and server port config + static file serve

--- server/src/web/server.js
+++ server/src/web/server.js
@@ -2,15 +2,17 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import WebApp from './WebApp'
 
+const port = process.env.PORT || 3000
+
 export default (Context, config) => {
   const server = express()
   server.use(bodyParser.urlencoded({ extended: false }))
   server.use(bodyParser.json())
-
+  server.use(express.static('public'))
   server.use(WebApp(Context, config))
 
   return {
     run: () =>
-      server.listen(3000, () => console.log('Listening on port 3000'))
+      server.listen(port, () => console.log(`Listening on port ${port}`))
   }
 }
