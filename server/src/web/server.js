fdf048755c25126ddefd65108d4b3046f02b60e0
Ivo Jesus <ivo.jesus@gmail.com>
2019-10-22 23:44:52 +0100

    Make public directory configurable

--- server/src/web/server.js
+++ server/src/web/server.js
@@ -3,18 +3,19 @@ import bodyParser from 'body-parser'
 import WebApp from './WebApp'
 import basicAuth from 'express-basic-auth'
 
-const port = process.env.PORT || 3000
-const username = process.env.HTTP_AUTH_USERNAME
-const password = process.env.HTTP_AUTH_PASSWORD
-
 export default (Context, config) => {
+  const port = config.env.PORT || 3000
+  const username = config.env.HTTP_AUTH_USERNAME
+  const password = config.env.HTTP_AUTH_PASSWORD
+  const publicFolder = config.env.PUBLIC_FOLDER || 'public'
+
   const server = express()
   server.use(bodyParser.urlencoded({ extended: false }))
   server.use(bodyParser.json())
 
   if (username && password)
     server.use(basicAuth({ users: { [username]: password }, challenge: true }))
-  server.use(express.static('public'))
+  server.use(express.static(publicFolder))
 
   server.use(WebApp(Context, config))
 
