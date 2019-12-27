5359fa41aeec59c54ea66efb6110939a51196c2e
Ivo Jesus <ivo.jesus@gmail.com>
2019-12-27 21:33:43 +0000

    UI implementation of the remove app functionality

--- server/src/Context.js
+++ server/src/Context.js
@@ -5,14 +5,15 @@ import RemindersRepo from './redis/reminders/RemindersRepo'
 import RemindersService from '#/src/core/reminders/RemindersService'
 import Messages from './messages'
 import SlackbotNotifier from './slackbot/SlackbotNotifier'
-// import TakeNotifier from './memory/TakeNotifier'
+import TakeNotifier from './memory/TakeNotifier'
 
 import Features from './core/features'
 
 const locator = Locator()
 locator.singleton(PromiseRedis.createClient({url: process.env['REDIS_URL']}), {name: 'redisClient'})
        .singleton(Messages, {name: 'messages'})
-       .singleton(new SlackbotNotifier(), { name: 'notifier' })
+       // .singleton(new SlackbotNotifier(), { name: 'notifier' })
+       .singleton(new TakeNotifier(), { name: 'notifier' })
        .singleton(new AppsRepo(locator.redisClient, process.env['ROOT_KEY']))
        .singleton(new RemindersRepo(locator.redisClient, process.env['ROOT_KEY']))
        .singleton(new RemindersService(locator.remindersRepo, locator.notifier, 4000))
