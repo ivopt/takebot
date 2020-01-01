3aa52d175fa0dfa8d618b703bec014ef9346ee67
Ivo Jesus <ivo.jesus@gmail.com>
2020-01-01 23:57:16 +0000

    Move HTTP AUTH to where it is needed

--- server/src/web/ReactApp.js
+++ server/src/web/ReactApp.js
@@ -1,8 +1,14 @@
 import path from 'path'
 import express, { Router } from 'express'
+import basicAuth from 'express-basic-auth'
 
 export default (_Context, config, router = new Router()) => {
   const publicFolder = config.env.PUBLIC_FOLDER || 'public'
+  const username = config.env.HTTP_AUTH_USERNAME
+  const password = config.env.HTTP_AUTH_PASSWORD
+
+  if (username && password)
+    router.use(basicAuth({ users: { [username]: password }, challenge: true }))
 
   router.use(express.static(publicFolder))
   router.get('*', (_req, res) => {
