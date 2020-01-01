a80a71fc4d341a7edd53dd88a67008429970f080
Ivo Jesus <ivo.jesus@gmail.com>
2020-01-01 23:52:45 +0000

    Remove the "initial" apps
    Apps should be configured, not part of ENV

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,6 @@ import './env.config'
 import Context from './Context'
 import server from './web/server'
 
-const apps = process.env["APPS"].split(",").map(name => ({name}))
-Context.appsRepo.add(...apps)
 const config = {
   env: process.env
 }
